0a490a77ac2d3ff57ff16fd6eebd82e96f0751f8
Thomas Smith <popadopz@gmail.com>
2013-11-05 14:47:06 -0700

    Basic IRC and chat command functions

---
+++ config.js
@@ -0,0 +1,10 @@
+{
+    "cyserver": "cytube server",
+    "cyuser": "username",
+    "cypw": "password",
+    "cychannel": "channel name",
+    "ircserver": "irc server",
+    "ircuser": "irc user",
+    "ircchannel": "irc channel",
+    "ircnickpass": "NSPassword"
+}
