2df4c0f217995873da9126de3f584a361b8c4c6c
Thomas Smith <popadopz@gmail.com>
2013-11-24 02:08:33 -0700

    adding videos

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@
 var io = require('socket.io-client');
 var fs = require('fs');
 var irc = require('irc');
+var MongoClient = require('mongodb').MongoClient;
 //logging
 var log = function(msg) {
 	d = new Date();
@@ -32,11 +33,14 @@ var log = function(msg) {
 var config = JSON.parse(fs.readFileSync('config.json','utf-8'));
 //Join server and channel
 var socket = io.connect(config.cyserver);
-socket.on('connect', function () {
+socket.on('connect', function (err) {
     socket.emit('login', { name: config.cyuser, pw: config.cypw });
     log("logging in...");
     socket.emit('joinChannel', { name: config.cychannel });
     log("joining channel...");
+    if(err) {
+    	log("cytube debug- " + err);
+    }
 });
 //get userlist
 var uList = [];
@@ -89,7 +93,20 @@ client.addListener('error', function(message) {
     log('error: ', message);
 });
 //
-//log messages and chat commands
+//connect to Database
+//
+var collection;
+MongoClient.connect("mongodb://localhost/rin", function(err, db) {
+	if(!err) {
+		log("DB- Database connected");
+		db.createCollection('yt', function(err, collection) {});
+		coll = db.collection('yt');
+	} else {
+		log("DB debug- " + err);
+	}
+});
+//
+//cytube messages
 //
 socket.on('chatMsg', function (message, callback) {
 	log("cytube- " + message.username + ": " + message.msg);
@@ -137,31 +154,28 @@ function commands(source, user, message){
 	//Roll
 	if(message.indexOf(config.commandchar + "roll") == 0) {
 		var roll = getRoll();
-		if(source == "cytube") {
 			socket.emit('chatMsg', {'msg': user + " you rolled " + roll + "!"});
-		}
-		else {
 			client.say(config.ircchannel, user + " you rolled " + roll + "!");
-		}
 	}
 	//help
 	if(message.indexOf(config.commandchar + "help") == 0) {
-		if(source == "cytube") {
-			socket.emit('chatMsg', {'msg': "Commands are: !greet, !bye, !ask, !roll, !source, !help"});
-		}
-		else {
-			client.say(config.ircchannel, "Commands are: !greet, !bye, !ask, !roll, !source, !help");
-		}
+			socket.emit('chatMsg', {'msg': "Commands are: !greet, !bye, !ask, !roll, !add(moderator), !source, !help"});
+			client.say(config.ircchannel, "Commands are: !greet, !bye, !ask, !roll, !add(moderator), !source, !help");
 	}
 	//source code
 	if(message.indexOf(config.commandchar + "source") == 0) {
-		if(source == "cytube") {
 			socket.emit('chatMsg', {'msg': "http://github.com/Twirlie/Rin"});
-		}
-		else {
 			client.say(config.ircchannel, "http://github.com/Twirlie/Rin");
-		}
 	}
+	if(message.indexOf(config.commandchar + "add" ) == 0){
+		add(message.substring(5).split(" "));
+	}
+	if(message.indexOf(config.commandchar + "addtest") == 0) {
+		add(message.substring(9).split(" "));
+	}
+	//
+	//functions
+	//
 	function test(args) {
 		if(args.length > 0) {
 			if(args[0] == "uList") {
@@ -187,29 +201,17 @@ function commands(source, user, message){
 			var roll = getRoll();
 			log("Rolled: " + roll);
 			if(roll > 5) {
-				if(source == "cytube"){
-					socket.emit('chatMsg', {'msg': "*" + args + ":* " + "Yes"});
-				}
-				else {
-					client.say(config.ircchannel, args + ": " + "Yes");
-				}
+				socket.emit('chatMsg', {'msg': "*" + args + ":* " + "Yes"});
+				client.say(config.ircchannel, args + ": " + "Yes");
 			}
 			else {
-				if(source == "cytube"){
-					socket.emit('chatMsg', {'msg': "*" + args + ":* " + "No"});
-				}
-				else {
-					client.say(config.ircchannel, args + ": " + "No");
-				}
+				socket.emit('chatMsg', {'msg': "*" + args + ":* " + "No"});
+				client.say(config.ircchannel, args + ": " + "No");
 			}
 		}
 		else {
-			if(source == "cytube"){
 				socket.emit('chatMsg', {'msg': "error: no arguments"});
-			}
-			else {
 				client.say(config.ircchannel, "error: no arguments");
-			}
 		}
 	}
 	function getRank(){
@@ -226,19 +228,99 @@ function commands(source, user, message){
 		return ranNum;
 	}
 	function greet(){
-		if(source == "cytube"){
 			socket.emit('chatMsg', {'msg': "Hello " + user + "."});
-		}
-		else{
 			client.say(config.ircchannel, "Hello " + user + ".");
-		}
 	}
 	function farewell(){
-		if(source == "cytube"){
 			socket.emit('chatMsg', {'msg': "Bye " + user + "."});
-		}
-		else{
 			client.say(config.ircchannel, "Bye " + user + ".");
+	}
+	function add(args) {
+		log("cytube debug- " + "addtest: " + args + "type: " + typeof args[0]);
+		var total = args[0].valueOf(), count = 0, vidData = [];
+		log("cytube debug- " + total);
+		if (total > 0) {
+			coll.find().toArray(function(err, docs) {
+				if(err) {
+					log("DB debug- " + err);
+				}
+				if(docs !== undefined) {
+					log("DB debug- " + "documents fetched");
+						for(var i=0;i<docs.length;i++) {
+							var roll = getRoll();
+							log("cytube debug- " + "rolling for random add: " + roll);
+							log("cytube debug- looping... " + "count: " + count);
+							if(count < total) {
+								if(roll > 5) {
+									var derping = {'vid': docs[i].vid, 'site': docs[i].type, 'pos': "end"};
+									vidData.push(derping);
+									count++;
+								}
+							} else {
+								log("cytube debug- vidData: " + JSON.stringify(vidData));
+								addMedia(vidData);
+								break;
+							};
+						}
+				}
+			});
 		}
 	}
+	function readDB(args) {
+		coll.find().toArray(function(err, docs) {
+			if(err) {
+				log("DB debug- " + err);
+			}
+		});
+	}
+	function addMedia(data){
+		var i = 0;
+			var interv = setInterval(function() {
+				if(data.length > i) {
+					var packet = {
+						"id": data[i].vid,
+						"type": data[i].site,
+						"pos": data[i].pos
+					}
+					socket.emit("queue", packet);
+					log("cytube debug- " + data[i].vid + " added");
+					i++;
+				} else {
+					clearInterval(interv);
+				}
+			}, 1500);
+	}
+}
+socket.on("queueFail", function(message){
+	log("cytube debug- " + JSON.stringify(message.msg));
+});
+socket.on("queue", function(message){
+	addToDB(message.item.media.id, message.item.media.type, message.item.queueby, message.item.media.title);
+});
+function addToDB(id, type, queueby, title) {
+ 	var doc = {"vid": id, "type": type, "user": queueby, "title": title, "forbidden": false};
+ 	coll.findOne({"vid": id}, function(err, result) {
+ 		if(err) {
+ 			log("DB debug- " + err);
+ 		}
+ 		if(result !== null) {
+	 		if(doc.user == result.user){
+	 			log("DB- " + doc.user + " already added this");
+	 		} else {
+	 			coll.insert(doc, {w:1}, function(err, result) {
+					if(err) {
+						log("DB debug- " + err);
+					}
+					log("DB- " + title + " added");
+				});
+	 		}
+	 	} else {
+ 			coll.insert(doc, {w:1}, function(err, result) {
+				if(err) {
+					log("DB debug- " + err);
+				}
+				log("DB- " + "\"" + title + "\"" + " added");
+			});
+ 		}
+ 	});
 }
